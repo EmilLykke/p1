bf23128637b573c46a62ff0d57a3a2d8d64686a3
Albert Hansen <Alberthansen2800@gmail.com>
2023-11-06 16:54:46 +0100

    Har tilføjet noget til scan funktionerne hhv prompt til rating samt number of passengers

--- scan_user_input.c
+++ scan_user_input.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
 void scan_user_input();
+
 int passenger_amount();
-void ratings();
+int ratings(int);
 void startpoint_and_destination();
 
-int main(void)
+void scan_user_input() // note: skal retunere rating, passenger antal og start-slut destination
 {
-    int number_of_passengers;
+    int number_of_passengers, co2_rating, price_rating, time_rating;
 
     number_of_passengers = passenger_amount();
+
+    co2_rating = ratings(1);
+    time_rating = ratings(2);
+    price_rating = ratings(3);
+
+    startpoint_and_destination();
 }
 
 int passenger_amount()
 {
     int amount;
-    int validInput;
 
     do
     {
-        printf("How many travelers are you planning to travel with? Between 1-100. (Including yourself)\n");
+        printf("How many travelers are you planning to travel with? Between 1-100. (Including yourself): ");
         scanf(" %d", &amount);
 
         if (amount > 0 && amount >= 100)
@@ -30,4 +36,39 @@ int passenger_amount()
     } while (amount > 0 && amount >= 100);
 
     return amount;
+}
+
+int ratings(int question)
+{
+    int answer;
+
+    printf("\n");
+
+    switch (question)
+    {
+    case 1:
+        printf("On a scale of 1-5, indicate the extent to which you prioritize CO2 emmesions: ");
+        break;
+
+    case 2:
+        printf("On a scale of 1-5, indicate the extent to which you prioritize travel time: ");
+        break;
+
+    case 3:
+        printf("On a scale of 1-5, indicate the extent to which you prioritize the price: ");
+        break;
+
+    default:
+        break;
+    }
+
+    scanf(" %d", &answer);
+
+    while (answer <= 0 || answer > 5)
+    {
+        printf("\nInvalid input, must an integer between 1-5: ");
+        scanf(" %d", &answer);
+    }
+
+    return answer;
 }
