2efdcaacd37dfe466b55ca3d9ba7f687b1ebeb44
Alb9ert <alberthansen2800@gmail.com>
2023-11-09 14:40:23 +0100

    main changes

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+
+void scan_user_input(int *number_of_passengers, int *co2_rating, int *time_rating, int *price_rating, char *start, char *end);
 
 int main(void)
 {
-    while ædru 
-    DRIK 
-    if "jeg er ikke fuld" 
-        Film med flash på oliver 
+    char startdestination[100];
+    char enddestination[100];
+    int co2_rating, time_rating, price_rating, number_of_passengers;
+
+    scan_user_input(&number_of_passengers, &co2_rating, &time_rating, &price_rating, startdestination, enddestination);
+
     return 0;
 }
