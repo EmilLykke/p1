ebff1fee76dcb377806aea8b78da50044a63016e
Emil Lykke <emillykkeg@gmail.com>
2023-11-12 19:52:56 +0100

    reading from file working

--- csv_read.c
+++ csv_read.c
@@ -1,1 +1,46 @@
+#include "functions.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char *splitString(char *line, int num)
+{
+    const char *tok;
+    for (tok = strtok(line, ";"); tok && *tok; tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+        {
+            return tok;
+        }
+    }
+    return NULL;
+}
+
+void csv_read()
+{
+    // opens flights file
+    FILE *stream = fopen("flights.csv", "r");
+    char line[1024];
+
+    // Read lines of flights file
+    while (fgets(line, 1024, stream))
+    {
+        char *tmp = strdup(line);
+
+        printf("%s\n", splitString(tmp, 3));
+        free(tmp);
+    }
+    fclose(stream);
+
+    // opens trains file
+    stream = fopen("trains.csv", "r");
+    // Read lines of trains file
+    while (fgets(line, 1024, stream))
+    {
+        char *tmp = strdup(line);
+
+        printf("%s\n", splitString(tmp, 3));
+        free(tmp);
+    }
+    fclose(stream);
+}
--- functions.h
+++ functions.h
@@ -1,3 +1,6 @@
 
-// Sacns user input
+// Scans user input
 void scan_user_input(int *, int *, int *, int *, char *, char *);
+
+// Read csv files
+void csv_read();
