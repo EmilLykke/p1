055dc5b31d902c78ea89ad6e89a722899b810eb4
Alb9ert <alberthansen2800@gmail.com>
2023-11-14 10:25:25 +0100

    toupper

--- scan_user_input.c
+++ scan_user_input.c
@@ -57,6 +57,9 @@ void startpoint_and_destination(char *start, char *end)
 
     printf("\nInput your end destination: ");
     scanf("%s", end);
+
+    toupper(start[0]);
+    toupper(end[0]);
 }
 void scan_user_input(int *number_of_passengers, int *co2_rating, int *time_rating, int *price_rating, char *start, char *end)
 {
