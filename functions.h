f0fb2e73fa4b111741f5cdcc4c2efff6db25f916
Emil Lykke <emillykkeg@gmail.com>
2023-11-15 19:47:21 +0100

    changes added

--- functions.h
+++ functions.h
@@ -1,4 +1,14 @@
 
+typedef struct Traveloption
+{
+    char *startDest;
+    char *endDest;
+    int travelTime;
+    double price;
+    int co2;
+    double distance;
+} Traveloption;
+
 // Scans user input
 void scan_user_input(int *, int *, int *, int *, char *, char *);
 
