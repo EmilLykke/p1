d4eb8b7f61c374e5d2cebbc40e064b545e6be924
Alb9ert <alberthansen2800@gmail.com>
2023-11-17 15:46:02 +0100

    pile fiks

--- find_route.c
+++ find_route.c
@@ -12,10 +12,10 @@ void find_route(Traveloption *airplane_or_train_option, char input_startDestinat
 
     for (int i = 0; i < size; i++)
     {
-        if (strcmp(airplane_or_train_option[i].startDest, input_startDestination) == 0 && strcmp(airplane_or_train_option[i].endDest, input_endDestination) == 0)
+        if (strcmp(airplane_or_train_option[i]->startDest, input_startDestination) == 0 && strcmp(airplane_or_train_option[i]->endDest, input_endDestination) == 0)
         {
 
-            newtime = airplane_or_train_option[i].traveltime;
+            newtime = airplane_or_train_option[i]->traveltime;
 
             while (1)
             {
@@ -27,7 +27,7 @@ void find_route(Traveloption *airplane_or_train_option, char input_startDestinat
                         if (newtime < time3)
                         {
                             newtime = time3;
-                            *index3 = i;
+                            *index1 = i;
                             break;
                         }
 
@@ -37,47 +37,47 @@ void find_route(Traveloption *airplane_or_train_option, char input_startDestinat
                     }
 
                     newtime = time1;
-                    *index1 = i;
+                    *index3 = i;
                     break;
                 }
             }
         }
     }
 
-    calculate_option(airplane_or_train_option, index1, index2, index3, option); // index 3 quickest, 2 2nd and index 1 is the slowest.
+    calculate_option(airplane_or_train_option, index1, index2, index3, option); // index 3 quickest, 2 2nd and index 1 is the slowest
 }
 
 void calculate_option(Traveloption *airplane_or_train_option, int index1, int index2, int index3, int option) // måske index1,2,3 pointer?
 {
 
-    if (option == AIRPLANE) // Præcise værdier skal ændres ud for rapporten. Særligt tag et kig på wastetime.
+    if (option == AIRPLANE) // Præcise værdier skal ændres ud for rapporten-> Særligt tag et kig på wastetime
     {
         // Calculates CO2 emmesion
-        airplane_or_train_option[index1].co2 = airplane_or_train_option[index1].distance * 246; // 246 grams of CO2 per KM on average for airplanes. (Figur 3)
-        airplane_or_train_option[index2].co2 = airplane_or_train_option[index2].distance * 246;
-        airplane_or_train_option[index3].co2 = airplane_or_train_option[index3].distance * 246;
+        airplane_or_train_option[index1]->co2 = airplane_or_train_option[index1]->distance * 246; // 246 grams of CO2 per KM on average for airplanes (Figur 3)
+        airplane_or_train_option[index2]->co2 = airplane_or_train_option[index2]->distance * 246;
+        airplane_or_train_option[index3]->co2 = airplane_or_train_option[index3]->distance * 246;
 
-        airplane_or_train_option[index1].wastetime = 165; // 165 minutes averaged on downtime for airplanes.
-        airplane_or_train_option[index2].wastetime = 165;
-        airplane_or_train_option[index3].wastetime = 165;
+        airplane_or_train_option[index1]->wastetime = 165; // 165 minutes averaged on downtime for airplanes
+        airplane_or_train_option[index2]->wastetime = 165;
+        airplane_or_train_option[index3]->wastetime = 165;
 
-        airplane_or_train_option[index1].totaltime = airplane_or_train_option[index1].wastetime + airplane_or_train_option[index1].traveltime; // traveltime + downtime
-        airplane_or_train_option[index2].totaltime = airplane_or_train_option[index2].wastetime + airplane_or_train_option[index2].traveltime;
-        airplane_or_train_option[index3].totaltime = airplane_or_train_option[index3].wastetime + airplane_or_train_option[index3].traveltime;
+        airplane_or_train_option[index1]->totaltime = airplane_or_train_option[index1]->wastetime + airplane_or_train_option[index1]->traveltime; // traveltime + downtime
+        airplane_or_train_option[index2]->totaltime = airplane_or_train_option[index2]->wastetime + airplane_or_train_option[index2]->traveltime;
+        airplane_or_train_option[index3]->totaltime = airplane_or_train_option[index3]->wastetime + airplane_or_train_option[index3]->traveltime;
     }
 
     else if (option == TRAIN)
     {
-        airplane_or_train_option[index1].co2 = airplane_or_train_option[index1].distance * 0; // 0 grams of CO2 per KM in German trains. ()
-        airplane_or_train_option[index2].co2 = airplane_or_train_option[index2].distance * 0;
-        airplane_or_train_option[index3].co2 = airplane_or_train_option[index3].distance * 0;
+        airplane_or_train_option[index1]->co2 = airplane_or_train_option[index1]->distance * 0; // 0 grams of CO2 per KM in German trains ()
+        airplane_or_train_option[index2]->co2 = airplane_or_train_option[index2]->distance * 0;
+        airplane_or_train_option[index3]->co2 = airplane_or_train_option[index3]->distance * 0;
 
-        airplane_or_train_option[index1].wastetime = 20; // average wastetime for train
-        airplane_or_train_option[index2].wastetime = 20;
-        airplane_or_train_option[index3].wastetime = 20;
+        airplane_or_train_option[index1]->wastetime = 20; // average wastetime for train
+        airplane_or_train_option[index2]->wastetime = 20;
+        airplane_or_train_option[index3]->wastetime = 20;
 
-        airplane_or_train_option[index1].totaltime = airplane_or_train_option[index1].wastetime + airplane_or_train_option[index1].traveltime; // traveltime + downtime
-        airplane_or_train_option[index2].totaltime = airplane_or_train_option[index2].wastetime + airplane_or_train_option[index2].traveltime;
-        airplane_or_train_option[index3].totaltime = airplane_or_train_option[index3].wastetime + airplane_or_train_option[index3].traveltime;
+        airplane_or_train_option[index1]->totaltime = airplane_or_train_option[index1]->wastetime + airplane_or_train_option[index1]->traveltime; // traveltime + downtime
+        airplane_or_train_option[index2]->totaltime = airplane_or_train_option[index2]->wastetime + airplane_or_train_option[index2]->traveltime;
+        airplane_or_train_option[index3]->totaltime = airplane_or_train_option[index3]->wastetime + airplane_or_train_option[index3]->traveltime;
     }
 }
